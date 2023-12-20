da4304079b4db38c547e47f90751c28365c27e21
Alex Shirazi <alexshirazi@Alexs-MacBook-Pro.local>
2023-12-20 11:13:45 -0800

    Init watch portion of app

---
+++ FitBlueprint/FitBlueprintWatch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FitBlueprintWatch Watch App
+//
+//  Created by Alex Shirazi on 12/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FitBlueprint/FitBlueprintWatch Watch App/FitBlueprintWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitBlueprintWatchApp.swift
+//  FitBlueprintWatch Watch App
+//
+//  Created by Alex Shirazi on 12/20/23.
+//
+
+import SwiftUI
+
+@main
+struct FitBlueprintWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
